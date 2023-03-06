ccc4a9ece96bf4d4ab48f5da7e15238af6a19b46
7Ji <pugokushin@gmail.com>
2023-03-06 23:20:05 +0800

    openvfd: also display dots in debugging mode

--- src/openvfd.c
+++ src/openvfd.c
@@ -100,10 +100,12 @@ void openvfd_write_report(char const report[5], bool const blink) {
     }
     pr_debug(
         "Characters:\n"
+            "\tdots: 0x%x 0b"FORMAT_BIT_SEQUANCE"\n"
             "\t1: %c 0x%x 0b"FORMAT_BIT_SEQUANCE"\n"
             "\t2: %c 0x%x 0b"FORMAT_BIT_SEQUANCE"\n"
             "\t3: %c 0x%x 0b"FORMAT_BIT_SEQUANCE"\n"
             "\t4: %c 0x%x 0b"FORMAT_BIT_SEQUANCE"\n", 
+            sequence.dots, expand_bit_sequance(sequence.dots),
             report[0], report[0], expand_bit_sequance(sequence.char_1),
             report[1], report[1], expand_bit_sequance(sequence.char_2),
             report[2], report[2], expand_bit_sequance(sequence.char_3),
