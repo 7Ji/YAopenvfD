2de25f8696b51453c29b1cba9f293f3e4f51b37b
7Ji <pugokushin@gmail.com>
2023-03-07 14:50:44 +0800

    reporter: move around

--- src/reporter.c
+++ src/reporter.c
@@ -30,53 +30,6 @@ enum reporter_type reporter_get_type_from_string(char const *const string) {
     return REPORTER_TYPE_NONE;
 }
 
-int reporter_prepare(struct reporter *const reporter_head) {
-    int r = openvfd_prepare();
-    if (r) {
-        pr_error("Failed to prepare OpenVFD: %d\n", r);
-        return 1;
-    }
-    for (struct reporter *reporter = reporter_head; reporter; reporter = reporter->next) {
-        if (collector_init(reporter->collector)) {
-            pr_error("Failed to init collector for reporter type %d(%s) duration %u\n", reporter->type, reporter_get_type_string(reporter->type), reporter->duration_second);
-            return 2;
-        } else {
-            pr_warn("Initialized collector for reporter type %d(%s) duration %u\n", reporter->type, reporter_get_type_string(reporter->type), reporter->duration_second);
-        }
-    }
-    return 0;
-}
-
-int reporter_loop(struct reporter *reporter_head) {
-    uint32_t word_last = 0;
-    uint8_t dots_last = 0;
-    while (true) {
-        for (struct reporter *reporter = reporter_head; reporter; reporter = reporter->next) {
-            unsigned remaining_second = reporter->duration_second;
-            bool blink = reporter->type == REPORTER_TYPE_TIME;
-            collector_prepare(reporter->collector);
-            while (true) {
-                sleep(1);
-                char buffer[5];
-                collector_report(reporter->collector, buffer);
-                uint32_t word_this = *(uint32_t *)buffer;
-                if (blink || word_this != word_last || reporter->dots != dots_last) {
-                    word_last = word_this;
-                    openvfd_write_report(word_this, reporter->dots, blink);
-                    pr_debug("Reporting type %s, remaining %u seconds, report content: %s\n", reporter_get_type_string(reporter->type), remaining_second, buffer);
-                } else {
-                    pr_debug("Omitted report type %s, remaining %u secons, report content: %s, since nothing changed\n", reporter_get_type_string(reporter->type), remaining_second, buffer);
-                }
-                if (reporter->duration_second &&  --remaining_second == 0) {
-                    break;
-                }
-            }
-            dots_last = reporter->dots;
-        }
-    }
-}
-
-
 #define REPORTER_PARSE_ARGUMENT_MAX_LEN 128
 #define REPORTER_PARSE_ARGUMENT_MAX_SEP 3
 
@@ -144,4 +97,50 @@ struct reporter *reporter_parse_argument(char const *const arg) {
     reporter->type = reporter_type;
     reporter->collector = collector;
     return reporter;
+}
+
+int reporter_prepare(struct reporter *const reporter_head) {
+    int r = openvfd_prepare();
+    if (r) {
+        pr_error("Failed to prepare OpenVFD: %d\n", r);
+        return 1;
+    }
+    for (struct reporter *reporter = reporter_head; reporter; reporter = reporter->next) {
+        if (collector_init(reporter->collector)) {
+            pr_error("Failed to init collector for reporter type %d(%s) duration %u\n", reporter->type, reporter_get_type_string(reporter->type), reporter->duration_second);
+            return 2;
+        } else {
+            pr_warn("Initialized collector for reporter type %d(%s) duration %u\n", reporter->type, reporter_get_type_string(reporter->type), reporter->duration_second);
+        }
+    }
+    return 0;
+}
+
+int reporter_loop(struct reporter *reporter_head) {
+    uint32_t word_last = 0;
+    uint8_t dots_last = 0;
+    while (true) {
+        for (struct reporter *reporter = reporter_head; reporter; reporter = reporter->next) {
+            unsigned remaining_second = reporter->duration_second;
+            bool blink = reporter->type == REPORTER_TYPE_TIME;
+            collector_prepare(reporter->collector);
+            while (true) {
+                sleep(1);
+                char buffer[5];
+                collector_report(reporter->collector, buffer);
+                uint32_t word_this = *(uint32_t *)buffer;
+                if (blink || word_this != word_last || reporter->dots != dots_last) {
+                    word_last = word_this;
+                    openvfd_write_report(word_this, reporter->dots, blink);
+                    pr_debug("Reporting type %s, remaining %u seconds, report content: %s\n", reporter_get_type_string(reporter->type), remaining_second, buffer);
+                } else {
+                    pr_debug("Omitted report type %s, remaining %u secons, report content: %s, since nothing changed\n", reporter_get_type_string(reporter->type), remaining_second, buffer);
+                }
+                if (reporter->duration_second &&  --remaining_second == 0) {
+                    break;
+                }
+            }
+            dots_last = reporter->dots;
+        }
+    }
 }
